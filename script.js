0cf5226ec3e85820f5a2660864a2f7434c9b257f
AlexLloyd89 <42323618+AlexLloyd89@users.noreply.github.com>
2018-12-09 21:44:57 -0500

    Style changes, got clone and drag/drop working...kind of

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-$(function(){
-  $("#postIt").draggable({
-    handle:  '.topBar',
-    }); 
-})
+jQuery(function() {
+    jQuery(".postIt").draggable({
+        //  use a helper-clone that is append to 'body' so is not 'contained' by a pane
+        helper: function() {
+            return jQuery(this).clone().appendTo('body').css({
+                'zIndex': 5
+            });
+        },
+        cursor: 'move',
+        containment: "document"
+    });
 
-$('button').on('click', function(){
-  $('#the-note').clone().appendTo($('body'))
-});
+    jQuery('#droppable').droppable({
+        activeClass: 'ui-state-hover',
+        accept: '.postIt',
+        drop: function(event, ui) {
+            if (!ui.draggable.hasClass("dropped"))
+                jQuery(this).append(jQuery(ui.draggable).clone().addClass("dropped").draggable());
+            }
+        });
+    });
+
